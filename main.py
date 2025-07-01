6ebb41d4657b11ede903b250abb9e176602e9bc1
giisampaio <160817191+giisampaio@users.noreply.github.com>
2025-07-01 14:24:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gif_generator import gerar_gif
 app = FastAPI()
 
 @app.post("/gerar-gif")
-async def gerar_gif_endpoint(
+async def criar_gif(
     map: UploadFile = File(...),
     clouds_agora: UploadFile = File(...),
     minus_60min: UploadFile = File(...),
@@ -14,7 +14,14 @@ async def gerar_gif_endpoint(
     minus_20min: UploadFile = File(...),
     minus_10min: UploadFile = File(...)
 ):
-    return await gerar_gif(
-        map, clouds_agora, minus_60min, minus_50min,
-        minus_40min, minus_30min, minus_20min, minus_10min
-    )
+    files = {
+        "map": map,
+        "clouds_agora": clouds_agora,
+        "minus_60min": minus_60min,
+        "minus_50min": minus_50min,
+        "minus_40min": minus_40min,
+        "minus_30min": minus_30min,
+        "minus_20min": minus_20min,
+        "minus_10min": minus_10min,
+    }
+    return gerar_gif(files)

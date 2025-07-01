773d7c49dfd99e63b995090346678aa824a92386
giisampaio <160817191+giisampaio@users.noreply.github.com>
2025-07-01 10:21:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI, UploadFile, File
-from gif_generator import gerar_gif
-import io
 from fastapi.responses import StreamingResponse
 from typing import List
+from gif_generator import gerar_gif
+import io
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "API online. Use POST /gerar-gif"}
-
 @app.post("/gerar-gif")
-async def criar_gif(files: List[UploadFile] = File(...)):
-    imagens = [await f.read() for f in files]
-    gif_bytes = gerar_gif(imagens)
+async def criar_gif(
+    map: UploadFile = File(...),
+    clouds_agora: UploadFile = File(...),
+    minus_60min: UploadFile = File(...),
+    minus_50min: UploadFile = File(...),
+    minus_40min: UploadFile = File(...),
+    minus_30min: UploadFile = File(...),
+    minus_20min: UploadFile = File(...),
+    minus_10min: UploadFile = File(...)
+):
+    files = {
+        "map": await map.read(),
+        "clouds_agora": await clouds_agora.read(),
+        "minus_60min": await minus_60min.read(),
+        "minus_50min": await minus_50min.read(),
+        "minus_40min": await minus_40min.read(),
+        "minus_30min": await minus_30min.read(),
+        "minus_20min": await minus_20min.read(),
+        "minus_10min": await minus_10min.read(),
+    }
+
+    gif_bytes = gerar_gif(files)
     return StreamingResponse(io.BytesIO(gif_bytes), media_type="image/gif")

744e2e92b24ccac639eb9e98b090690c49b862aa
giisampaio <160817191+giisampaio@users.noreply.github.com>
2025-07-01 14:22:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import StreamingResponse
-from typing import List
 from gif_generator import gerar_gif
-import io
 
 app = FastAPI()
 
 @app.post("/gerar-gif")
-async def criar_gif(
+async def gerar_gif_endpoint(
     map: UploadFile = File(...),
     clouds_agora: UploadFile = File(...),
     minus_60min: UploadFile = File(...),
@@ -17,16 +14,7 @@ async def criar_gif(
     minus_20min: UploadFile = File(...),
     minus_10min: UploadFile = File(...)
 ):
-    files = {
-        "map": await map.read(),
-        "clouds_agora": await clouds_agora.read(),
-        "minus_60min": await minus_60min.read(),
-        "minus_50min": await minus_50min.read(),
-        "minus_40min": await minus_40min.read(),
-        "minus_30min": await minus_30min.read(),
-        "minus_20min": await minus_20min.read(),
-        "minus_10min": await minus_10min.read(),
-    }
-
-    gif_bytes = gerar_gif(files)
-    return StreamingResponse(io.BytesIO(gif_bytes), media_type="image/gif")
+    return await gerar_gif(
+        map, clouds_agora, minus_60min, minus_50min,
+        minus_40min, minus_30min, minus_20min, minus_10min
+    )

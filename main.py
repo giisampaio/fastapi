0fd7f69679a9208d36f9a91999ea5e0af17380a6
giisampaio <160817191+giisampaio@users.noreply.github.com>
2025-07-01 10:17:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI, UploadFile, File
 from gif_generator import gerar_gif
 import io
 from fastapi.responses import StreamingResponse
+from typing import List
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "API online. Use POST /gerar-gif"}
+
 @app.post("/gerar-gif")
-async def criar_gif(files: list[UploadFile] = File(...)):
+async def criar_gif(files: List[UploadFile] = File(...)):
     imagens = [await f.read() for f in files]
     gif_bytes = gerar_gif(imagens)
     return StreamingResponse(io.BytesIO(gif_bytes), media_type="image/gif")
